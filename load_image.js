39e8cc5c4b0cbe46f6863b3a4dd920774e26a850
yu2mimi <yu2.39c5bb@gmail.com>
2025-04-07 11:21:17 +0900

    json読み込みとJSでの画像の追加

--- load_image.js
+++ load_image.js
@@ -4,29 +4,28 @@ fetch('assets/json/data.json')
     const image_content = document.getElementById("image-content");
 
     data.forEach((photo) => {
-      console.log(photo.id);
       const div = document.createElement("div");
-      div.class = "image-content";
+      div.className = "image-box";
 
       const input = document.createElement("input");
       input.type = "checkbox";
-      input.class = "toggle";
+      input.className = "toggle";
       input.id = `toggle${photo.id}`;
 
       const img = document.createElement("img");
-      img.src = photo.url;
+      img.src = `assets/image/gallery/${photo.url}`;
       img.alt = `Photo${photo.id}`;
-      img.class = "image";
+      img.className = "image";
       img.style.objectPosition = photo.object_position;
 
       const label = document.createElement("label");
-      label.class = "img-label";
+      label.className = "img-label";
       label.for = `toggle${photo.id}`;
 
       div.appendChild(input);
       div.appendChild(img);
       div.appendChild(label);
-    });
 
-    image_content.appendChild(img)
+      image_content.appendChild(div)
+    });
   });
